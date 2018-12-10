0a3d1729e10275a51f4072ef9da2952d7f51eb40
pniroula <pravesh.niroula@mrcooper.com>
2018-12-09 20:19:45 -0600

    Added a table to show the status of PIDs requested to Move Forward

--- src/containers/MoveForwardDashboard/MoveForwardDashboard.jsx
+++ src/containers/MoveForwardDashboard/MoveForwardDashboard.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import axios from 'axios';
 
 class MoveForwardDashboard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { pids: '' };
+    this.state = { pids: '', pidsStatus: {} };
 
     this.handleChange = this.handleChange.bind(this);
     this.moveForward = this.moveForward.bind(this);
@@ -19,6 +24,7 @@ class MoveForwardDashboard extends React.Component {
 
   moveForward() {
     const { pids } = this.state;
+    const pidsStatus = {};
     const pidsList = pids.split(',');
     const requestURL = 'https://mrcooperdev.azure-api.net/enterprise/wq/activate/api/process/release';
     const requestBody = {
@@ -32,15 +38,23 @@ class MoveForwardDashboard extends React.Component {
     axios.post(requestURL, requestBody, { headers })
       .then((res) => {
         console.log(res);
+        Object.keys(res).forEach((key) => {
+          pidsStatus[key] = res[key].updateInstanceStatusResponse.statusCode;
+        });
+        this.setState({ pidsStatus });
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }
 
   render() {
     const { pids } = this.state;
+    const { pidsStatus } = this.state;
     return (
       <>
         <Grid container>
-          <Grid item lg={3} xs={3}>
+          <Grid lg={3} xs={3}>
             <Grid xs={12}>
               <TextField
                 id="pids"
@@ -48,7 +62,7 @@ class MoveForwardDashboard extends React.Component {
                 margin="normal"
                 multiline
                 onChange={this.handleChange}
-                rows="4"
+                rows="8"
                 style={{ width: '100%' }}
                 value={pids}
               />
@@ -56,11 +70,31 @@ class MoveForwardDashboard extends React.Component {
             <Button
               className="material-ui-button"
               color="primary"
+              margin="normal"
               onClick={this.moveForward}
               variant="contained"
             >
               Move Forward
             </Button>
+          </Grid>
+          <Grid item lg={9} xs={9}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>PIDs</TableCell>
+                  <TableCell>Status</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                { Object.keys(pidsStatus).map(keyName => (
+                  <TableRow>
+                    <TableCell>{ keyName }</TableCell>
+                    <TableCell>{ pidsStatus[keyName] }</TableCell>
+                  </TableRow>
+                ))
+                }
+              </TableBody>
+            </Table>
           </Grid>
         </Grid>
       </>
