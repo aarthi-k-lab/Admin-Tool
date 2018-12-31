7ed28e17ac74b54222d142431d0f6cf867b02a24
venkteshv <venktesh.viswanathan@mrcooper.com>
2018-12-31 17:34:32 +0530

    added default values for now for airbrake to avoid errors when config not present

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -6,8 +6,8 @@ class ErrorBoundary extends React.Component {
     super(props);
     this.state = { hasError: false };
     this.airbrake = new AirbrakeClient({
-      projectId: process.env.AIRBRAKE_PROJECT_ID,
-      projectKey: process.env.AIRBRAKE_PROJECT_KEY,
+      projectId: process.env.AIRBRAKE_PROJECT_ID || 197023,
+      projectKey: process.env.AIRBRAKE_PROJECT_KEY || 'd6c029a4c7cb7019f0f089bb113fd803',
     });
     this.sessionValue = { session: (Math.random() * 1e20).toString(36) };
   }
