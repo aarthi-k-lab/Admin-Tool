b0bd6bd22494ce4f5550332571b1b8f9baf93b89
Shreya.Gupta@mrcooper.com <Shreya.Gupta@mrcooper.com>
2019-02-14 16:13:16 +0530

    added waterfall, sts, fldd, last doc fields in loan tombstone

--- src/models/LoanTombstone/LoanTombstone.js
+++ src/models/LoanTombstone/LoanTombstone.js
@@ -21,6 +21,28 @@ function generateTombstoneItem(title, content) {
   };
 }
 
+function waterfallLookup(id) {
+  switch (id) {
+    case 1: return 'Non-GSE/Default Waterfall';
+    case 2: return 'FHA Waterfall';
+    case 3: return 'VA/USDA Waterfall';
+    case 4: return 'DHHL/PHA Waterfall';
+    case 5: return 'FNMA Waterfall';
+    case 6: return 'FHLMC Waterfall';
+    case 7: return 'HFS Waterfall';
+    case 8: return 'Special Servicing 1 Waterfall';
+    case 9: return 'Special Servicing 2 Waterfall';
+    case 10: return 'Non-GSE/Non-Delegated Waterfall';
+    case 11: return 'BoNY Waterfall';
+    case 12: return 'USAA Waterfall';
+    case 13: return 'Disaster Waterfall';
+    case 14: return 'State Alternative Review Waterfall';
+    case 15: return 'USAA HE Loan / HELOC';
+    case 16: return 'Z Deal Waterfall';
+    default: return NA;
+  }
+}
+
 function getLoanItem(loanDetails) {
   const loanNumber = getOr('loanNumber', loanDetails, NA);
   return generateTombstoneItem('Loan #', loanNumber);
@@ -108,9 +130,31 @@ function getWaterfallId(_, evalDetails) {
   return generateTombstoneItem('Waterfall ID', waterfallId);
 }
 
+function getWaterfallName(_, evalDetails) {
+  const waterfallId = getOr('waterfallId', evalDetails, NA);
+  return generateTombstoneItem('Waterfall Name', waterfallLookup(waterfallId));
+}
+
 function getModificationType(_, evalDetails) {
   const modificationType = getOr('resolutionChoiceType', evalDetails, NA);
   return generateTombstoneItem('Modification Type', modificationType);
+}
+
+function getLastDocumentReceivedDate(_, evalDetails) {
+  const date = moment(evalDetails.lastDocumentReceivedDate);
+  const dateString = date.isValid() ? date.format('MM/DD/YYYY') : NA;
+  return generateTombstoneItem('Last Document Received Date', dateString);
+}
+
+function getFLDD(_, evalDetails) {
+  const date = moment(evalDetails.fldd);
+  const dateString = date.isValid() ? date.format('MM/DD/YYYY') : NA;
+  return generateTombstoneItem('FLDD Date', dateString);
+}
+
+function getSuccessorInInterestStatus(loanDetails) {
+  const successorInInterestStatus = getOr('successorInInterestStatus', loanDetails, NA);
+  return generateTombstoneItem('Successor in Interest Status', successorInInterestStatus);
 }
 
 function getTombstoneItems(loanDetails, evalDetails) {
@@ -125,7 +169,11 @@ function getTombstoneItems(loanDetails, evalDetails) {
     getUPBItem,
     getNextPaymentDueDateItem,
     getWaterfallId,
+    getWaterfallName,
     getModificationType,
+    getSuccessorInInterestStatus,
+    getLastDocumentReceivedDate,
+    getFLDD,
   ];
   const data = dataGenerator.map(fn => fn(loanDetails, evalDetails));
   return data;
