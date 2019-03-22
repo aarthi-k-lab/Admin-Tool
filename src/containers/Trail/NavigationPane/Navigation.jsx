04d22536890d94498eed34447515e55d5bf36972
Sudhan M <Sudhan.M@mrcooper.com>
2019-03-22 17:44:19 +0530

    Navigation card changes

--- src/containers/Trail/NavigationPane/Navigation.jsx
+++ src/containers/Trail/NavigationPane/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import CardIcon from '@material-ui/icons/Person';
 import PropTypes from 'prop-types';
 import './Navigation.css';
 
@@ -13,69 +14,65 @@ class Navigation extends React.PureComponent {
   handleClick() {
     const { onCardClick } = this.props;
     onCardClick();
-    console.log('-----------------');
   }
 
   render() {
     const { navigationList } = this.props;
     return (
       <>
-        <div styleName="navigation">
-          {navigationList.map(parent => (
-            <div style={{ padding: '15px 0px 15px 0px' }}>
-              <Grid container onClick={() => this.handleClick()} styleName="parent-grid">
-                <Grid item xs={2}>
-                  Img
+        {navigationList.map(parent => (
+          <div styleName="cards">
+            <div styleName="parent-card">
+              <Grid container onClick={() => this.handleClick()} styleName="main-container">
+                <Grid item styleName="image" xs={1}>
+                  <CardIcon styleName="icon" />
                 </Grid>
-                <Grid item xs={7}>
+                <Grid item styleName="user-detail" xs={7}>
                   <span styleName="value-style">{parent.header}</span>
                   <br />
                   <span styleName="header-style">{parent.assignee}</span>
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item styleName="right-item" xs={4}>
                   <span styleName={parent.status === 'FAILED' ? 'failedStatus' : 'completedStatus'}>{parent.status}</span>
-                  <br />
-                  <span styleName="header-style">In 67 days</span>
+                  <span styleName="header-style align">In 67 days</span>
                 </Grid>
-                <br />
-                <br />
-                <br />
-                <br />
-                <Grid item xs={3}>
+              </Grid>
+              <Grid container onClick={() => this.handleClick()} styleName="sub-container">
+                <Grid item styleName="item left-item" xs={3}>
                   <span styleName="header-style">START DATE</span>
                   <br />
                   <span styleName="value-style">{parent.startDate}</span>
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item styleName="item" xs={3}>
                   <span styleName="header-style">END DATE</span>
                   <br />
                   <span styleName="value-style">{parent.endDate}</span>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item styleName="item" xs={6}>
                   <span styleName="header-style">EXPECTED COMPLETION DATE</span>
                   <br />
                   <span styleName="value-style">{parent.expectedCompletionDate}</span>
                 </Grid>
               </Grid>
-              {parent.child && parent.child.map(child => (
-                <div style={{ padding: '15px 0px 10px 40px' }}>
-                  <Grid container styleName="child-grid">
-                    <Grid item xs={9}>
-                      <span styleName="value-style">{child.header}</span>
-                      <br />
-                      <span styleName="header-style">{child.assignee}</span>
-                    </Grid>
-                    <Grid item xs={3}>
-                      <span styleName="header-style">{child.status}</span>
-                      <br />
-                      <span styleName="header-style">{child.statusDate}</span>
-                    </Grid>
-                  </Grid>
-                </div>
-              ))}
             </div>
-          ))}
-        </div>
+            {parent.child && parent.child.map(child => (
+              <div style={{ padding: '15px 0px 10px 40px' }}>
+                <Grid container styleName="child-grid">
+                  <Grid item xs={9}>
+                    <span styleName="value-style">{child.header}</span>
+                    <br />
+                    <span styleName="header-style">{child.assignee}</span>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <span styleName="header-style">{child.status}</span>
+                    <br />
+                    <span styleName="header-style">{child.statusDate}</span>
+                  </Grid>
+                </Grid>
+              </div>
+            ))}
+          </div>
+        ))}
       </>
     );
   }
