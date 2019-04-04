9fc918b65219868e3f8e6dbe2f24d9cbe70258b8
Sudhan M <Sudhan.M@mrcooper.com>
2019-04-04 16:36:04 +0530

    test commit

--- src/containers/LoanActivity/Status/Status.test.jsx
+++ src/containers/LoanActivity/Status/Status.test.jsx
@@ -16,10 +16,43 @@ describe('<Navigation />', () => {
   ];
 
   const navigationListWithNoData = [];
+  const classes = {
+    connectorDisabled: {
+      padding: 0,
+    },
+    connectorLine: {
+      minHeight: 'auto',
+    },
+    inactiveIconContainer: {
+      position: 'relative',
+      top: '70px',
+      left: '-6px',
+    },
+    activeIconContainer: {
+      position: 'relative',
+      top: '70px',
+      left: '-8px',
+    },
+    inactiveIcon: {
+      color: '#9e9e9e !important',
+      width: '12px',
+    },
+    activeIcon: {
+      color: '#9e9e9e !important',
+      width: '18px',
+    },
+    iconText: {
+      display: 'none !important',
+    },
+    labelRoot: {
+      marginLeft: '12px',
+      display: 'none',
+    },
+  };
   it('shows Navigation with data', () => {
     const { Status } = TestHooks;
     const wrapper = shallow(
-      <Status statusList={navigationList} />,
+      <Status classes={classes} statusList={navigationList} />,
     );
     expect(wrapper.find('div')).toHaveLength(navigationList.length * 2);
   });
@@ -27,7 +60,7 @@ describe('<Navigation />', () => {
   it('shows Navigation with no data', () => {
     const { Status } = TestHooks;
     const wrapper = shallow(
-      <Status statusList={navigationListWithNoData} />,
+      <Status classes={classes} statusList={navigationListWithNoData} />,
     );
     expect(wrapper.find('div')).toHaveLength(0);
   });
