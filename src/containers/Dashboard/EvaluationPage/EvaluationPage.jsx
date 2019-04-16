4e7dea26f7a6e575b99146de31884ab39a5a6df9
Haritha <haritha.msk@mrcooper.com>
2019-04-16 15:18:44 +0530

    Ram/Haritha - Snack bar added

--- src/containers/Dashboard/EvaluationPage/EvaluationPage.jsx
+++ src/containers/Dashboard/EvaluationPage/EvaluationPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as R from 'ramda';
+import { connect } from 'react-redux';
 import ContentHeader from 'components/ContentHeader';
 import FullHeightColumn from 'components/FullHeightColumn';
 import Controls from 'containers/Controls';
@@ -10,6 +12,8 @@ import TasksAndChecklist from 'containers/Dashboard/TasksAndChecklist';
 import LoanActivity from 'containers/LoanActivity';
 import DashboardModel from 'models/Dashboard';
 import { withRouter } from 'react-router-dom';
+import CustomSnackBar from 'components/CustomSnackBar';
+import { selectors as notificationSelectors, operations as notificationOperations } from 'ducks/notifications';
 import './EvaluationPage.css';
 
 function isNotLoanActivity(group) {
@@ -32,6 +36,24 @@ class EvaluationPage extends React.PureComponent {
     }
   }
 
+  renderSnackBar() {
+    const { group, snackBarData, closeSnackBar } = this.props;
+    const page = DashboardModel.PAGE_LOOKUP.find(pageInstance => pageInstance.group === group);
+    const hasCommentsWidget = !R.isNil(page) ? page.hasCommentsWidget : false;
+    if (hasCommentsWidget) {
+      return (
+        <CustomSnackBar
+          message={snackBarData && snackBarData.message}
+          onClose={closeSnackBar}
+          open={snackBarData && snackBarData.open}
+          type={snackBarData && snackBarData.type}
+        />
+      );
+    }
+
+    return null;
+  }
+
   render() {
     const { location, group } = this.props;
     const el = DashboardModel.PAGE_LOOKUP.find(page => page.path === location.pathname);
@@ -48,6 +70,7 @@ class EvaluationPage extends React.PureComponent {
         </ContentHeader>
         <Tombstone />
         <FullHeightColumn styleName="columns-container">
+          {this.renderSnackBar()}
           {this.renderDashboard()}
         </FullHeightColumn>
       </>
@@ -60,16 +83,28 @@ EvaluationPage.defaultProps = {
 };
 
 EvaluationPage.propTypes = {
+  closeSnackBar: PropTypes.func.isRequired,
   group: PropTypes.string,
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
   }).isRequired,
+  snackBarData: PropTypes.node.isRequired,
 };
+
+const mapStateToProps = state => ({
+  snackBarData: notificationSelectors.getSnackBarState(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  closeSnackBar: notificationOperations.closeSnackBar(dispatch),
+});
+
+const EvaluationPageContainer = connect(mapStateToProps, mapDispatchToProps)(EvaluationPage);
 
 const TestHooks = {
   EvaluationPage,
 };
 
-export default withRouter(EvaluationPage);
+export default withRouter(EvaluationPageContainer);
 
 export { TestHooks };
