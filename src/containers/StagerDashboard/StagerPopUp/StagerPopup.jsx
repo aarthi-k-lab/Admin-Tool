07662f06554b4899ac1d35dd001cb57b206a99f3
Amarnath Bhamidipaty <Amarnath.Bhamidipaty@mrcooper.com>
2019-05-15 13:18:47 +0530

    code refractor and added close button to popup

--- src/containers/StagerDashboard/StagerPopUp/StagerPopup.jsx
+++ src/containers/StagerDashboard/StagerPopUp/StagerPopup.jsx
@@ -1,10 +1,12 @@
-/* eslint-disable  */
-// eslint-disable
+/* eslint-disable no-nested-ternary */
+
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import Divider from '@material-ui/core/Divider';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import RemoveRedEyeIcon from '@material-ui/icons/RemoveRedEye';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -23,52 +25,27 @@ class StagerPopup extends React.PureComponent {
     super(props);
     this.state = {
       showSuccess: false,
-      checkedBoxes: [],
-    }
-    this.getTotalloanCount = this.getTotalloanCount.bind(this);
+      checkedData: [],
+      isPopupClose: true,
+    };
+    this.getTotalLoanCount = this.getTotalLoanCount.bind(this);
     this.handleCheckbox = this.handleCheckbox.bind(this);
+    this.onCloseClick = this.onCloseClick.bind(this);
   }
 
   onEyeIconClick() {
     this.setState(prevState => ({
-      showSuccess: !prevState.showSuccess
+      showSuccess: !prevState.showSuccess,
     }));
   }
 
-  getTotalloanCount() {
-    const { popupData, action } = this.props;
-    this.totalCount = 0;
-    popupData.forEach((dataValue) => {
-      if (!dataValue.error) {
-        this.successCount = dataValue.data.length;
-      }
-      this.totalCount += dataValue.data.length;
-    });
-    this.failureCount = this.totalCount - this.successCount;
-    return (` ${this.successCount} / ${this.totalCount} Loans ordered successfully [${action}]`);
-  }
-
-  handleCheckbox(e, s) {
-    const checkedBoxes = [...this.state.checkedBoxes];
-    if (e.target.checked) {
-      checkedBoxes.push(s)
-    } else {
-      const index = checkedBoxes.findIndex((ch) => ch.evalId === s.evalId);
-      checkedBoxes.splice(index, 1);
-    }
-    this.setState({ checkedBoxes });
-  }
   onRetryClick() {
-    const { checkedBoxes } = this.state;
-  }
-
-  onRetryClick() {
-    const { checkedBoxes } = this.state;
+    const { checkedData } = this.state;
     const { triggerDocsOutCall, action } = this.props;
     const docsOutPayload = R.map(dataUnit => ({
-      evalId: dataUnit['evalId'] && dataUnit['evalId'].toString(),
+      evalId: dataUnit.evalId && dataUnit.evalId.toString(),
       taskId: dataUnit.taskId && dataUnit.taskId.toString(),
-    }), checkedBoxes);
+    }), checkedData);
     const payload = {
       type: 'DOCSOUT STAGER',
       data: docsOutPayload,
@@ -76,84 +53,129 @@ class StagerPopup extends React.PureComponent {
     triggerDocsOutCall(payload, action);
   }
 
+  onCloseClick() {
+    const { onClearDocsOutAction } = this.props;
+    this.setState({
+      isPopupClose: false,
+    });
+    onClearDocsOutAction();
+  }
+
+  getTotalLoanCount() {
+    const { popupData, action } = this.props;
+    this.totalLoansCount = 0;
+    popupData.forEach((dataValue) => {
+      if (!dataValue.error) {
+        this.succeedLoancount = dataValue.data.length;
+      }
+      this.totalLoansCount += dataValue.data.length;
+    });
+    this.failedLoancount = this.totalLoansCount - this.succeedLoancount;
+    if (this.failedLoancount === 0) {
+      setTimeout(() => {
+        this.onCloseClick();
+      }, 2000);
+    }
+    return (` ${this.succeedLoancount} / ${this.totalLoansCount} Loans ordered successfully [${action}]`);
+  }
+
+  handleCheckbox(event, loanDetails) {
+    const { checkedData } = this.state;
+    if (event.target.checked) {
+      checkedData.push(loanDetails);
+    } else {
+      const index = checkedData.findIndex(data => data.evalId === loanDetails.evalId);
+      checkedData.splice(index, 1);
+    }
+    this.setState({ checkedData });
+  }
+
   render() {
     const { popupData } = this.props;
-    const { showSuccess, checkedBoxes } = this.state;
+    const { showSuccess, checkedData, isPopupClose } = this.state;
     return (
-      <div>
-        <ExpansionPanel styleName="card-header">
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon styleName="card-header-title" />} styleName="card-title">
-            <Typography styleName="card-header-title">
-              {this.getTotalloanCount()}
+      <div styleName={isPopupClose ? 'open' : 'close'}>
+        <ExpansionPanel styleName="expansion-header">
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon styleName="expansion-header-title" />} styleName="expansion-title">
+            <Typography styleName="expansion-header-title">
+              {this.getTotalLoanCount()}
             </Typography>
           </ExpansionPanelSummary>
 
           {popupData.map(loanDetails => (
             <>
-              <ExpansionPanelDetails styleName={!loanDetails.error ? 'card-success-title' : 'card-failure-title'}>
+              <ExpansionPanelDetails styleName={!loanDetails.error ? 'expansion-succeedLoan-title' : 'expansion-failedLoan-title'}>
                 <Grid
                   container
                 >
-                  <Grid item xs={1} style={{ paddingBottom: '0.5rem'}}>
-                    <span styleName={!loanDetails.error ? 'sucessedloan' : 'failedloan'}>
-                      {!loanDetails.error ? this.successCount : this.failureCount}
+                  <Grid item style={{ paddingBottom: '0.5rem' }} xs={1}>
+                    <span styleName={!loanDetails.error ? 'succeedloan' : 'failedloan'}>
+                      {!loanDetails.error ? this.succeedLoancount : this.failedLoancount}
                     </span>
                   </Grid>
-                <Grid item xs={10}>
-                  <span styleName="loans-font">{loanDetails.error ? 'Loans failed' : 'Loans ordered successfully'}</span>
-                </Grid>
-                <Grid item xs={1}>
-                  <span styleName="loans-font">
-                    {!loanDetails.error ? (<div styleName='viewHideIcon'><RemoveRedEyeIcon onClick={() => this.onEyeIconClick()} styleName="eyeicon" /></div>) : (
-                      (loanDetails.error && loanDetails.data.length !== 0) ?
-                        (
-                          <div styleName='retry'>
-                             <Button color="primary" variant="contained" onClick={() => this.onRetryClick()} >
+                  <Grid item xs={10}>
+                    <span styleName="popup-font">{loanDetails.error ? 'Loans failed' : 'Loans ordered successfully'}</span>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <span styleName="popup-font">
+                      {!loanDetails.error ? (<div styleName="viewHideIcon"><RemoveRedEyeIcon onClick={() => this.onEyeIconClick()} styleName="eyeicon" /></div>) : (
+                        (loanDetails.error && loanDetails.data.length !== 0)
+                          ? (
+                            <div styleName="retry">
+                              <Button color="primary" onClick={() => this.onRetryClick()} variant="contained">
                                 Retry
-                            </Button>
-                          </div>
-                        ) : null
-
-                    )}
-                  </span>
+                              </Button>
+                            </div>
+                          ) : null
+                      )}
+                    </span>
+                  </Grid>
                 </Grid>
-                </Grid>
-            </ExpansionPanelDetails>
-
+              </ExpansionPanelDetails>
               <div styleName={loanDetails.error ? 'failed' : `success${showSuccess ? 'View' : 'Hide'}`}>
                 {
                   loanDetails.data.map(loanArrayDetails => (
-                    <ExpansionPanelDetails styleName="card-failure-title">
+                    <ExpansionPanelDetails styleName="expansion-failedLoan-title">
                       <Grid
                         container
                       >
                         <Grid item xs={1}>
-                          {loanDetails.error ? (<Checkbox onChange={(e) => this.handleCheckbox(e, loanArrayDetails)}
-                            id={loanArrayDetails.evalId}
-                            checked={checkedBoxes.find((ch) => ch.evalId === loanArrayDetails.evalId)}
-                            style={{ height: '15px', padding: '0px' }} />) : null}
+                          {loanDetails.error ? (
+                            <Checkbox
+                              checked={checkedData.find(
+                                data => data.evalId === loanArrayDetails.evalId,
+                              )}
+                              id={loanArrayDetails.evalId}
+                              onChange={event => this.handleCheckbox(event, loanArrayDetails)}
+                              style={{ height: '15px', padding: '0px' }}
+                            />
+                          ) : null}
                         </Grid>
                         <Grid item xs={4}>
-                          <span styleName="loans-font">{loanArrayDetails.evalId}</span>
+                          <span styleName="popup-font">{loanArrayDetails.evalId}</span>
                         </Grid>
                         <Grid item xs={7}>
                           {loanDetails.error ? (
                             <>
                               <WarningIcon style={{ fontSize: '1.5rem', marginRight: '0.5rem' }} styleName="alert-font" />
-                              <span style={{ position: 'relative', top: '-4px' }} styleName="loans-font alert-font">
+                              <span style={{ position: 'relative', top: '-4px' }} styleName="popup-font alert-font">
                                 {loanArrayDetails.taskId}
                               </span>
                             </>
                           ) : null}
                         </Grid>
                       </Grid>
-                  </ExpansionPanelDetails>
-                ))}
-            </div>
+                    </ExpansionPanelDetails>
+                  ))}
+              </div>
 
             </>
-        ))
-        }
+          ))
+          }
+          <Divider />
+          <ExpansionPanelActions style={{ padding: '0.5rem' }}>
+            <Button onClick={() => this.onCloseClick()} size="small" styleName="popup-font">Close</Button>
+          </ExpansionPanelActions>
         </ExpansionPanel>
       </div>
     );
@@ -165,17 +187,19 @@ StagerPopup.defaultProps = {
 };
 
 StagerPopup.propTypes = {
-  operationDetails: PropTypes.node.isRequired,
-  triggerDocsOutCall: PropTypes.func.isRequired,
+  action: PropTypes.string.isRequired,
+  onClearDocsOutAction: PropTypes.func.isRequired,
   popupData: PropTypes.arrayOf(
     PropTypes.shape({
       error: PropTypes.bool,
     }),
   ),
+  triggerDocsOutCall: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = dispatch => ({
   triggerDocsOutCall: stagerOperations.triggerDocsOutCall(dispatch),
+  onClearDocsOutAction: stagerOperations.onClearDocsOutAction(dispatch),
 });
 
 export default connect(null, mapDispatchToProps)(StagerPopup);
