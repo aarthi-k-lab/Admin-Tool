cb51bf4149ccde6f8be5b759e2b682ee29f78c99
Haritha <haritha.msk@mrcooper.com>
2019-05-29 18:42:10 +0530

    BE checklist icon disabled

--- src/lib/RouteAccess.js
+++ src/lib/RouteAccess.js
@@ -49,13 +49,13 @@ const feuwTasksAndChecklist = {
   img: '/static/img/fe_beta.svg',
   groups: ['allaccess', 'feuw-beta', 'beta'],
 };
-
-const beuwTasksAndChecklist = {
-  path: '/backend-checklist',
-  name: 'backend-checklist',
-  img: '/static/img/be_beta.svg',
-  groups: ['allaccess', 'beuw-beta', 'beta'],
-};
+// commented for demo purpose
+// const beuwTasksAndChecklist = {
+//   path: '/backend-checklist',
+//   name: 'backend-checklist',
+//   img: '/static/img/be_beta.svg',
+//   groups: ['allaccess', 'beuw-beta', 'beta'],
+// };
 
 const checkListGroups = {
   groups: ['allaccess', 'feuw-beta', 'beta', 'proc', 'proc-mgr'],
@@ -75,7 +75,7 @@ const links = [
   frontendUnderwriter,
   feuwTasksAndChecklist,
   backendUnderwriter,
-  beuwTasksAndChecklist,
+  // beuwTasksAndChecklist,
   stager,
   moveForward,
   loanActivity,
@@ -106,9 +106,9 @@ function hasFrontendChecklistAccess(groups) {
   return hasGroup(feuwTasksAndChecklist.groups, groups);
 }
 
-function hasBackendChecklistAccess(groups) {
-  return hasGroup(beuwTasksAndChecklist.groups, groups);
-}
+// function hasBackendChecklistAccess(groups) {
+//   return hasGroup(beuwTasksAndChecklist.groups, groups);
+// }
 
 function hasChecklistAccess(groups) {
   return hasGroup(checkListGroups.groups, groups);
@@ -143,7 +143,7 @@ module.exports = {
   hasBackendUnderwriterAccess,
   hasDocProcessorAccess,
   hasFrontendUnderwriterAccess,
-  hasBackendChecklistAccess,
+  // hasBackendChecklistAccess,
   hasFrontendChecklistAccess,
   hasManagerDashboardAccess,
   hasMoveForwardAccess,
