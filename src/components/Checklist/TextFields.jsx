9fddd212f1bf8b06ca0c40da07d963e1ac6447ad
Haritha <haritha.msk@mrcooper.com>
2019-07-03 12:07:01 +0530

    Disable the option for selecting future dates

--- src/components/Checklist/TextFields.jsx
+++ src/components/Checklist/TextFields.jsx
@@ -5,12 +5,48 @@ import React from 'react';
 import HTMLElements from '../../constants/componentTypes';
 import './TextFields.css';
 
+
+function getCurrentDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let dt = date.getDate();
+  if (dt < 10) {
+    dt = `0${dt}`;
+  }
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  return `${year}-${month}-${dt}`;
+}
+
+function getProps(type, props) {
+  const { MULTILINE_TEXT, DATE, NUMBER } = HTMLElements;
+  switch (type) {
+    case DATE: {
+      return { ...props, inputProps: { type: 'date', max: getCurrentDate() } };
+    }
+    case MULTILINE_TEXT: {
+      return {
+        ...props, maxRows: 10, multiline: true, rows: 5,
+      };
+    }
+    case NUMBER: {
+      return {
+        ...props,
+        inputProps: {
+          type: 'number',
+          min: '0',
+        },
+      };
+    }
+    default: return { ...props };
+  }
+}
+
 const TextFields = (props) => {
-  const { MULTILINE_TEXT } = HTMLElements;
   const { type, title } = props;
-  const properties = type !== MULTILINE_TEXT ? props : {
-    ...props, maxRows: 10, multiline: true, rows: 5,
-  };
+  const properties = getProps(type, props);
   return (
     <FormControl component="fieldset">
       <FormLabel component="legend" styleName="text-label">{title}</FormLabel>
