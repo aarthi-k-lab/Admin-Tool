6a65e6b1bb685a9990eb72f2ed1144dc72a3e2f2
Ashwin Ramamurthy <Ashwin.Ramamurthy@nationstarmail.com>
2019-11-11 10:59:16 +0000

    Merged PR 38121: Fixed checklist access bug
    Fixed checklist access bug
    
    Related work items: #345505

--- src/state/ducks/dashboard/sagas.js
+++ src/state/ducks/dashboard/sagas.js
@@ -10,7 +10,6 @@ import {
 } from 'redux-saga/effects';
 import * as R from 'ramda';
 import * as Api from 'lib/Api';
-import RouteAccess from 'lib/RouteAccess';
 import { actions as tombstoneActions } from 'ducks/tombstone/index';
 import { actions as commentsActions } from 'ducks/comments/index';
 import { selectors as loginSelectors } from 'ducks/login/index';
@@ -301,7 +300,7 @@ function* fetchChecklistDetails(checklistId) {
 
 function* shouldRetriveChecklist(searchItem) {
   const groupList = yield select(loginSelectors.getGroupList);
-  const hasChecklistAccess = RouteAccess.hasChecklistAccess(groupList);
+  const hasChecklistAccess = groupList.includes(R.path(['payload', 'group'], searchItem).toLowerCase());
   const taskName = R.path(['payload', 'taskName'], searchItem);
   const isChecklistTask = CHECKLIST_TASKNAMES.includes(taskName);
   const retriveChecklist = hasChecklistAccess && isChecklistTask;
