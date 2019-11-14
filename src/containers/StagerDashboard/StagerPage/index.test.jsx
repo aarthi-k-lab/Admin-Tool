f49ab6a93eabf396dbab5fe412d17f28905f42e0
Praveen M15 <Praveen.M15@mrcooper.com>
2019-11-14 20:44:34 +0530

    test case fix

--- src/containers/StagerDashboard/StagerPage/index.test.jsx
+++ src/containers/StagerDashboard/StagerPage/index.test.jsx
@@ -10,7 +10,7 @@ describe('<StagerPage />', () => {
     const wrapper = shallow(
       <TestExports.StagerPage groups={groups} stager="ALL_STAGER" triggerStagerValue={triggerStagerValue} />,
     );
-    expect(wrapper.find('ContentHeader')).toHaveLength(1);
+    expect(wrapper.find('ContentHeader')).toHaveLength(0);
     expect(wrapper.find('WithStyles(ForwardRef(Grid))')).toHaveLength(3);
     expect(wrapper.find('Connect(StagerDetailsTable)')).toHaveLength(1);
     expect(wrapper.find('StagerTiles')).toHaveLength(1);
