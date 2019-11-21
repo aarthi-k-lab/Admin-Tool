66031881be10aa4f2757444bd405d95eb8cd8e42
Haritha <haritha.msk@mrcooper.com>
2019-11-21 13:19:23 +0530

    Bulk Order Submit button fix

--- config/webpack.local.js
+++ config/webpack.local.js
@@ -66,7 +66,7 @@ const config = merge(
           },
         },
         '/api/stager': {
-          target: 'https://cmodstagerdev.int.mrcooper.io',
+          target: 'https://cmodstagerqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -74,7 +74,7 @@ const config = merge(
           },
         },
         '/api/disposition': {
-          target: 'https://cmoddispositiondev.int.mrcooper.io',
+          target: 'https://cmoddispositionqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -82,7 +82,7 @@ const config = merge(
           },
         },
         '/api/workassign': {
-          target: 'https://cmodworkassigndev.int.mrcooper.io',
+          target: 'https://cmodworkassignqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -90,7 +90,7 @@ const config = merge(
           },
         },
         '/api/search-svc': {
-          target: 'https://cmodsearchenginedev.int.mrcooper.io',
+          target: 'https://cmodsearchengineqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -98,7 +98,7 @@ const config = merge(
           },
         },
         '/api/userskills': {
-          target: 'https://cmoduserskilldev.int.mrcooper.io',
+          target: 'https://cmoduserskillqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -106,7 +106,7 @@ const config = merge(
           },
         },
         '/api/release': {
-          target: 'https://cmodactivateservdev.int.mrcooper.io',
+          target: 'https://cmodactivateservqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -114,7 +114,7 @@ const config = merge(
           },
         },
         '/api/tkams': {
-          target: 'https://cmodtkamsdev.int.mrcooper.io',
+          target: 'https://cmodtkamsqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -122,7 +122,7 @@ const config = merge(
           },
         },
         '/api/bpm-audit': {
-          target: 'https://cmodauditbpmeventsdev.int.mrcooper.io',
+          target: 'https://cmodauditbpmeventsqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -130,7 +130,7 @@ const config = merge(
           },
         },
         '/api/task-engine': {
-          target: 'https://cmodtaskenginedev.int.mrcooper.io',
+          target: 'http://localhost:7600',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -138,7 +138,7 @@ const config = merge(
           },
         },
         '/api/utility': {
-          target: 'https://cmodutilitydev.int.mrcooper.io',
+          target: 'https://cmodutilityqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
@@ -162,7 +162,7 @@ const config = merge(
           },
         },
         '/api/dataservice': {
-          target: 'https://cmoddataservdev.int.mrcooper.io',
+          target: 'https://cmoddataservqa.int.mrcooper.io',
           secure: false,
           changeOrigin: true,
           pathRewrite: {
