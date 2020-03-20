fdb0812dc0a8a1bbd63e8cc803727527a840e004
Gowtham K <Gowtham.K@mrcooper.com>
2020-03-20 14:31:16 +0530

    Changed reUploadFile components to grid components

--- src/containers/Dashboard/Covius/ReUploadFile.jsx
+++ src/containers/Dashboard/Covius/ReUploadFile.jsx
@@ -3,6 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import PropTypes from 'prop-types';
 import './ReUploadFile.css';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 // const handleChange = () => {
 //   const { onChange } = this.props;
@@ -12,40 +13,51 @@ import Button from '@material-ui/core/Button';
 const ReUploadFile = ({ fileName, onChange }) => (
   <>
     <div>
-      <div styleName="msgblock">
-        <img alt="submit_error_icon" src="/static/img/default_selected_green_small.svg" styleName="largeIcon" />
-        <span styleName="reuploadmsg">Excel document uploaded Successfully.</span>
-      </div>
-      <div styleName="msgblock1">
-        <TextField
-          InputProps={{
-            readOnly: true,
-            style: {
-              fontSize: '1.4rem',
-              padding: '5.5px 5.5px !important',
-              fontStyle: 'italic',
-            },
-            disableUnderline: 'true',
-          }}
-          styleName="fileTextBox"
-          value={fileName}
-        />
-        <Button
-          color="secondary"
-          component="label"
-          onClick={onChange}
-          styleName="deletebtn"
-          variant="contained"
-        >
+      <Grid alignItems="center" container direction="column" styleName="msgblock" xs={12}>
+        <Grid alignItems="center" container direction="row" justify="flex-end" xs={12}>
+          <Grid alignItems="flex-start" container direction="row" item justify="flex-end" xs={5}>
+            <img alt="submit_error_icon" src="/static/img/default_selected_green_small.svg" styleName="largeIcon" />
+          </Grid>
+          <Grid item xs={7}>
+            <span styleName="reuploadmsg">Excel document uploaded Successfully.</span>
+          </Grid>
+        </Grid>
+        <Grid alignItems="center" container direction="column" justify="center" xs={12}>
+          <Grid item xs={12}>
+            <TextField
+              InputProps={{
+                readOnly: true,
+                style: {
+                  fontSize: '1.4rem',
+                  padding: '5.5px 5.5px !important',
+                  fontStyle: 'italic',
+                },
+                disableUnderline: 'true',
+              }}
+              styleName="fileTextBox"
+              value={fileName}
+            />
+          </Grid>
+        </Grid>
+        <Grid container direction="column" item xs={12}>
+          <Grid item xs={12}>
+            <Button
+              component="label"
+              onClick={onChange}
+              styleName="deletebtn"
+              variant="contained"
+            >
           DELETE
-        </Button>
-      </div>
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   </>
 );
 
 ReUploadFile.defaultProps = {
-  fileName: 'hello.xls',
+  fileName: 'hedfgsdfgdfgdsgdfgdsfgdsfgllo.xls',
 };
 
 ReUploadFile.propTypes = {
