42b85716df221ace0902536366b31932390b65cd
Sudhan M <Sudhan.M@mrcooper.com>
2020-07-16 08:17:17 +0000

    Merged PR 58805: Eslint issue fix

--- src/containers/Dashboard/DocsIn/DocsIn.jsx
+++ src/containers/Dashboard/DocsIn/DocsIn.jsx
@@ -443,7 +443,8 @@ class DocsIn extends React.PureComponent {
     const { loansNumber } = this.state;
     const { onLoansSubmit, onFailedLoanValidation, bulkOrderPageType } = this.props;
     if (validateLoanFormat(loansNumber)) {
-      const loanNumbers = loansNumber.trim().replace(/\n/g, ',').split(',').map(s => s.trim()).filter(s => s.length > 0 );
+      const loanNumbers = loansNumber.trim().replace(/\n/g, ',').split(',').map(s => s.trim())
+        .filter(s => s.length > 0);
       const loanNumbersList = new Set(loanNumbers);
       const payload = {
         loanNumbers: [...loanNumbersList],
@@ -468,7 +469,8 @@ class DocsIn extends React.PureComponent {
     } = this.props;
     let statusName = '';
     if (validateLoanFormat(loansNumber)) {
-      const loanNumbers = loansNumber.trim().replace(/\n/g, ',').split(',').map(s => s.trim()).filter(s => s.length > 0 );
+      const loanNumbers = loansNumber.trim().replace(/\n/g, ',').split(',').map(s => s.trim())
+        .filter(s => s.length > 0);
       const inputValueList = new Set(loanNumbers);
       if (selectedStagerTaskOptions) {
         statusName = selectedStagerTaskOptions;
