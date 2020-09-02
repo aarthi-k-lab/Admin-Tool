ae27287f0cb29d9ad1853c5c33577bac4c8ee360
Ashwin Ramamurthy <Ashwin.Ramamurthy@nationstarmail.com>
2020-09-02 05:10:01 +0000

    Merged PR 63874: Added check for manager access for rendering userReport
    Added check for manager access for rendering userReport
    
    Related work items: #466035

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { selectors as dashboardSelectors, operations } from 'ducks/dashboard';
-// import LandingPage from './LandingPage';
+import * as R from 'ramda';
+import LandingPage from './LandingPage';
 import EvaluationPage from './EvaluationPage';
 import UserReport from './UserReport';
+import { selectors as loginSelectors } from '../../state/ducks/login';
 
-function getPage(isFirstVisit, group) {
-  // return isFirstVisit && group !== 'LA' ? <LandingPage /> : <EvaluationPage group={group} />;
-  return isFirstVisit && group !== 'LA' ? <UserReport /> : <EvaluationPage group={group} />;
+function getPage(isFirstVisit, group, userGroupList) {
+  const homepage = R.contains(`${group.toLowerCase()}-mgr`, userGroupList) ? <UserReport /> : <LandingPage />;
+  return isFirstVisit && group !== 'LA' ? homepage : <EvaluationPage group={group} />;
 }
 
 function Dashboard(props) {
-  const { isFirstVisit, group, onGetGroupName } = props;
+  const {
+    isFirstVisit, group, onGetGroupName, userGroupList,
+  } = props;
   onGetGroupName(group);
-  return getPage(isFirstVisit, group);
+  return getPage(isFirstVisit, group, userGroupList);
 }
 
 const TestExports = {
@@ -23,6 +27,7 @@ const TestExports = {
 
 const mapStateToProps = state => ({
   isFirstVisit: dashboardSelectors.isFirstVisit(state),
+  userGroupList: loginSelectors.getGroupList(state),
 });
 
 Dashboard.defaultProps = {
@@ -32,6 +37,7 @@ Dashboard.propTypes = {
   group: PropTypes.string,
   isFirstVisit: PropTypes.bool.isRequired,
   onGetGroupName: PropTypes.func.isRequired,
+  userGroupList: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 const mapDispatchToProps = dispatch => ({
   onGetGroupName: operations.onGetGroupName(dispatch),
