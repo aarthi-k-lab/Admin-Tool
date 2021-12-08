a2567ef12ec0eccb46bfbdb7a6ba28520d711a5c
Haritha M S K <Haritha.MSK@mrcooper.com>
2021-12-08 15:54:05 +0000

    Merged PR 111583: Mod/Report Handling
    ![image.png](https://mrcooper.visualstudio.com/47f033da-9a3e-42a3-aece-8c3a4d8cfaf2/_apis/git/repositories/246bf4d3-0bf6-4ecb-902c-5c21382b521a/pullRequests/111583/attachments/image.png)
    
    Related work items: #639382

--- src/containers/ManagerDashboard/DropDownSelect.jsx
+++ src/containers/ManagerDashboard/DropDownSelect.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import * as R from 'ramda';
 
 class DropDownSelect extends Component {
   constructor(props) {
@@ -19,7 +20,10 @@ class DropDownSelect extends Component {
   renderDropDown() {
     const { getDashboardItems, selectedValue } = this.props;
     const getDashboardItemsReports = getDashboardItems.length > 0
-      ? [getDashboardItems[0], getDashboardItems[1], getDashboardItems[2], getDashboardItems[3]]
+      ? R.filter(
+        reportDetails => reportDetails.isManagerReport,
+        getDashboardItems,
+      )
       : [];
     return (
       <FormControl>
